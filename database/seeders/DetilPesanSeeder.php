8b6bca61f0f5dc42d532511fe98ad60fcde980a0
Dafa Faturahman <dafafaturamanshidiq@example.com>
2026-04-09 20:56:59 +0700

    update: Mengupdate untuk seeder DetilBarang

--- database/seeders/DetilPesanSeeder.php
+++ database/seeders/DetilPesanSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\DetilPesan;
 
 class DetilPesanSeeder extends Seeder
 {
@@ -12,6 +13,16 @@ class DetilPesanSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data =[
+            ['id_pesan' => 1001, 'id_produk' => 'PR001', 'jumlah' => 1, 'harga' =>8500000],
+            ['id_pesan' => 1001, 'id_produk' => 'PR002', 'jumlah' => 2, 'harga' =>150000],
+            ['id_pesan' => 1002, 'id_produk' => 'PR003', 'jumlah' => 1, 'harga' =>350000],
+            ['id_pesan' => 1003, 'id_produk' => 'PR005', 'jumlah' => 3, 'harga' =>80000],
+            ['id_pesan' => 1005, 'id_produk' => 'PR004', 'jumlah' => 1, 'harga' =>2100000]
+        ];
+
+        foreach($data as $item){
+            DetilPesan::create($item);
+        };
     }
 }
