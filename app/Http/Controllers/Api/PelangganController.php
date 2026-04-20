8dc2f805fa1d5f7c5b9a8e381bc7285e7a77d0ff
Dafa Faturahman <dafafaturamanshidiq@example.com>
2026-04-20 19:02:04 +0700

    feat: Add show method to PelangganController for retrieving customer details

--- app/Http/Controllers/Api/PelangganController.php
+++ app/Http/Controllers/Api/PelangganController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
-use App\Models\Pelanggan; // import model Pelanggan
+use App\Models\Pelanggan;
+use Validator;// import model Pelanggan
 
 class PelangganController extends Controller
 {
+    // Daftar Pelanggan
     public function index()
     {
         //mengembalikan data dalam format json
@@ -15,6 +18,58 @@ class PelangganController extends Controller
             'success' => true,
             'message' => 'Daftar Pelanggan',
             'data' => Pelanggan::all()
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        $pelanggan = Pelanggan::find($id);
+
+        if($pelanggan){ // jika pelanggan tidak ada
+            return response()->json([
+                'success' => true,
+                'message' => 'Detail data Pelanggan',
+                'data' => $pelanggan
+            ], 200);
+        }
+
+        // jika data tidak ditemukan
+        return response()->json([
+            'success' => false,
+            'message' => 'Data tidak Ditemukan'
+        ], 404);
+    }
+
+    public function store(Request $request)
+    {
+
+    //validasi
+        $validator = Validator::make($request->all(), [
+            'id_pelanggan' => 'required|unique:pelanggan,id_pelanggan',
+            'nm_pelanggan' => 'required',
+            'alamat' => 'required',
+            'telepon' => 'required',
+            'email' => 'required|email|unique:pelanggan,email'
+        ]);
+
+        // jika validasi gagal
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        //insert
+        $pelanggan = Pelanggan::create([
+            'id_pelanggan' => $request->id_pelanggan,
+            'nm_pelanggan' => $request->nm_pelanggan,
+            'alamat' => $request->alamat,
+            'telepon' => $request->telepon,
+            'email' => $request->email
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Berhasil Ditambahkan',
+            'data' => $pelanggan
         ], 200);
     }
 }
